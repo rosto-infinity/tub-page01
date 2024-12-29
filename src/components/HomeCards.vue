2e1e4dc15fccc11c4c2afa4702cfcdd09be61b90
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-29 21:25:01 +0100

    Card
    Card

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -5,7 +5,7 @@ import Card from './Card.vue';
 
 
 <template>
-   <!-- Developers and Employers -->
+   <!-- Développeurs and Employeurs -->
    <section class="py-4"> 
       <div class="container-xl lg:container m-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
