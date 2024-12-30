7707f7abc493a719978194e206d8d271c4471b44
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-30 22:28:28 +0100

    createRouter
    createRouter

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+   {  path: '/',
+     path: '/home',
+     component: HomeView,
+     },
+  ],
+})
+
+export default router
