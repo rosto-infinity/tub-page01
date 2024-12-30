ed9aae3ec3047ed1f1056215e575976d314b8d79
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-30 06:36:58 +0100

    xw
     wx

---
+++ src/components/JobListings.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import JobData from './jobs.json'; 
+  import { ref } from 'vue';
+
+  const jobs =ref(JobData);
+  console.log(jobs.value[0].company.description);
+  
+</script>
+
+
+<template>
+  Job listings
+</template>
+
+
