889b13ac423a7ed28e90f1ac0e3fa7915f0dcedc
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-30 13:25:24 +0100

    Job Listing 1
    Job Listing 1

---
+++ src/components/JobListing.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+
+<template>
+<!-- Job Listing 1 -->
+<div class="relative rounded-xl bg-white shadow-md">
+            <div class="p-4">
+              <div class="mb-6">
+                <div class="my-2 text-gray-600">À temps plein</div>
+                <h3 class="text-xl font-bold">Développeur Vue Senior</h3>
+              </div>
+
+              <div class="mb-5">
+                Nous recherchons un développeur front-end talentueux pour rejoindre notre équipe à Boston, MA. Le candidat idéal aura de solides compétences en HTML, CSS et JavaScript...
+              </div>
+
+              <h3 class="mb-2 text-green-500">70 000 $ à 80 000 $ par an</h3>
+
+              <div class="mb-5 border border-gray-100"></div>
+
+              <div class="mb-4 flex flex-col justify-between lg:flex-row">
+                <div class="mb-3 text-orange-700">
+                  <i class="fa-solid fa-location-dot text-lg"></i>
+                  Boston, Massachusetts
+                </div>
+                <a
+                  href="job.html"
+                  class="h-[36px] rounded-lg bg-green-500 px-4 py-2 text-center text-sm text-white hover:bg-green-600"
+                >
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+</template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -10,14 +10,12 @@ const jobs = ref(JobData);
 
 <template>
   <section class="bg-blue-50 px-4 py-10">
-    <div class="container-xl lg:container mx-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Parcourir les offres d'emploi</h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <div class="container-xl mx-auto lg:container">
+      <h2 class="mb-6 text-center text-3xl font-bold text-green-500 underline" >Parcourir les offres d'emploi</h2>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
         <div v-for="job in jobs" :key="job.id">
           {{ job.title }}
-
         </div>
-
       </div>
     </div>
   </section>
